bc78732fb758ab31a66dd70870a902d0adf365f4
Alex Laursen <ajlaursen@aol.com>
2021-05-10 13:25:08 -0400

    fixing folder structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-color">
+      <div className="fg-color">
+        <div className="main-color">
+          <div className="text-color">asdad</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
