16a2e5a5f5c06276fb59344824a2778ba191bb53
Parker Morgan <jpmaster13@msn.com>
2021-05-10 15:55:09 -0600

    Deleted everything im sad

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './pages/Login'
-import { Home } from './pages/Home';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+
+
+
+
 
 function App() {
+  
   return (
-    <div className="main">
-      <Home/>
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={(props) => (
+          <Home {...props} />
+        )}
+        />
+        
+      </Switch>
+    </Router>
   );
 }
 
