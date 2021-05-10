7699764bc6c69d038109813c0630d9f41bc8e5d7
Alex Laursen <ajlaursen@aol.com>
2021-05-10 18:17:40 -0400

    updates storage with new score

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Home } from './pages/Home';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+
 
 
 
